caf16e1542476a492c3324dc62751dc7494b7f5f
Rodolphe Asséré <rodolphe1.assere@epitech.eu>
2017-03-17 16:49:22 +0100

    [API Meteo] Add date and time fields manually from the station

--- data/models.py
+++ data/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 
 SENSOR_CHOICES = (
-    ('u', 'unknown'),
-    ('h', 'hygrometry'),
-    ('r', 'rain'),
-    ('t', 'temperature'),
-    ('wd', 'wind_direction'),
-    ('ws', 'wind_strength'),
-    ('s', 'snow'),
+    ('unknown', 'u'),
+    ('hygrometry', 'h'),
+    ('rain', 'r'),
+    ('temperature', 't'),
+    ('wind_direction', 'wd'),
+    ('wind_strength', 'ws'),
+    ('snow', 's'),
+    ('pressure', 'p'),
 )
 
 class Sensor(models.Model):
     """
     Sensor is an atomic data pushed from a meteo station
     """
-    sensor = models.CharField(choices=SENSOR_CHOICES, default=SENSOR_CHOICES[0][1], max_length=100, help_text="type of the sensor")
+    sensor = models.CharField(choices=SENSOR_CHOICES, default=SENSOR_CHOICES[0][0], max_length=100, help_text="type of the sensor")
     value = models.CharField(default='N/A', max_length=100, help_text="Floating number or wind direction")
-    date = models.DateField(auto_now_add=True, help_text="Field automatically fill with current date")
-    time = models.TimeField(auto_now_add=True, help_text="Field automatically fill with current time")
+    date = models.DateField(help_text="Date format: YYYY-MM-DD", blank=False)
+    time = models.TimeField(help_text="Time format: HH:MM:SS", blank=False)
     station_id = models.IntegerField(default=-1, help_text="Station identifier where the data comes from")
-
+    #owner = models.ForeignKey('auth.User', related_name='sensor', on_delete=models.CASCADE)
+    
 
 class Station(models.Model):
     """
@@ -29,3 +31,4 @@ class Station(models.Model):
     description = models.CharField(default='N/A', max_length=400, help_text="Station details")
     latitude = models.FloatField(default=500)
     longitude = models.FloatField(default=500)
+    #owner = models.ForeignKey('auth.User', related_name='station', on_delete=models.CASCADE)
--- data/serializers.py
+++ data/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from data.models import Sensor, Station
+#from django.contrib.auth.models import User
 
 class SensorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,12 @@ class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
         fields = ('id', 'name', 'description', 'latitude', 'longitude')
+
+# Work in progress on authentication system
+#class UserSerializer(serializers.ModelSerializer):
+#    station = serializers.PrimaryKeyRelatedField(many=True, queryset=Station.objects.all())
+#    sensor = serializers.PrimaryKeyRelatedField(many=True, queryset=Sensor.objects.all())
+#    
+#    class Meta:
+#        model = User
+#        fields = ('id', 'username', 'station', 'sensor')
